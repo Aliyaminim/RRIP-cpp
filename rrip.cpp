3265da1d0e6a5334dca12956947b2bc5da8b41a4
Minimullina Aliya <minimullina.ae@phystech.edu>
2023-09-22 00:29:08 +0300

    modified after debugging

--- rrip.cpp
+++ rrip.cpp
@@ -2,29 +2,27 @@
 #include <cassert>
 #include "rrip.hpp"
 
-int slow_get_page_int(int page) {
-    return page;
+int slow_get_page_int(int key) {
+    return key;
 }
 
 int main() {
-    int hits = 0;
     size_t m; //cache size
     int n;    //number of upcoming elements
 
     std::cin >> m >> n;
     assert(std::cin.good());
     caches<int, int> c(m);
-    //vector<T> elements; //for ideal cache 
-    //unordered_map <T, pair<int, list <int>>> data;
+
+    int hits = 0;
 
     for (int i = 0; i < n; ++i) {
         int q;
         std::cin >> q;
         assert(std::cin.good());
-        //elements.push_back(q);
         
-        if (c.lookup_update(q))
-            hits += 1;
+        if (c.lookup_update(q, slow_get_page_int))
+            hits++;
 
         //c.print_cache();
     }
--- rrip.hpp
+++ rrip.hpp
@@ -12,9 +12,9 @@ template <typename T, typename KeyT> class caches {
 
 private :
 
-static const int RRIPval_DIST = 3;
-static const int RRIPval_LONG = 2;
-static const int RRIPval_NEAR = 0;
+    static const int RRIPval_DIST = 3;
+    static const int RRIPval_LONG = 2;
+    static const int RRIPval_NEAR = 0;
 
     struct cache_node {
         KeyT key;
@@ -26,11 +26,9 @@ static const int RRIPval_NEAR = 0;
 
     std::list<cache_node> cache_;
     using ListIt = typename std::list<cache_node>::iterator;
-    std::unordered_map<KeyT, ListIt> hash_;
+    ListIt fst_dist = cache_.end(); 
 
-    cache_node* fst_dist = nullptr;;
-    ListIt fst_dist_it;
-    ListIt cache_back;
+    std::unordered_map<KeyT, ListIt> hash_;
 
 public:
 
@@ -38,59 +36,60 @@ public:
 
     bool full() const { return (cache_.size() == sz_); }
 
-    bool lookup_update(T value) {
-        auto node = new cache_node;
-        node->key = value; //?
-        node->value = value; 
-        node->rrip = RRIPval_LONG;
+    bool lookup_update(KeyT key, T (*slow_get_page)(KeyT)) {
+        T value = slow_get_page(key);
+        cache_node node = {key, value, RRIPval_LONG};
 
-        auto hit = hash_.find(node->key); 
+        auto hit = hash_.find(node.key); 
 
         if (hit == hash_.end()) {
             if (full()) {
-                if (fst_dist == nullptr) {
-                    cache_back = std::next(cache_.end(), -1);
+                if (fst_dist == cache_.end()) {
+                    ListIt cache_back = std::prev(cache_.end());
                     int i = RRIPval_DIST - cache_back->rrip;
                     for (auto k = cache_.begin(); k != cache_.end(); k++) {
                         k->rrip += i;
-                        if ((k->rrip == RRIPval_DIST) && (fst_dist == nullptr)) {
-                            fst_dist = &(*k);
-                            fst_dist_it = k;
+                        if ((k->rrip == RRIPval_DIST) && (fst_dist == cache_.end())) {
+                            fst_dist = k;
                         }
                     }
                 }
-                ListIt help = next(fst_dist_it);
+                ListIt second_dist = std::next(fst_dist);
                 hash_.erase(fst_dist->key);      
-                cache_.erase(fst_dist_it); 
+                cache_.erase(fst_dist); //when erase happens, iterator???
 
-                cache_.emplace(help, *node);
-                //help--;
-                hash_.emplace(node->key, help);
+                cache_.emplace(second_dist, node);
+                ListIt node_it = std::prev(second_dist);
+                hash_.emplace(node.key, node_it);
 
-                //help++;
-                fst_dist_it = help;
-                fst_dist = &(*fst_dist_it);
+                fst_dist = second_dist;
                 return false;
             } 
 
-            cache_.emplace_front(*node);
-            hash_.emplace(node->key, cache_.begin());
+            cache_.emplace_back(node);
+            ListIt cache_back = std::prev(cache_.end());
+            hash_.emplace(node.key, cache_back);
             return false;
         } 
+
         auto eltit = hit->second;
+        if (eltit == fst_dist)
+            fst_dist = std::next(fst_dist);
+
         if (eltit != cache_.begin())
             cache_.splice(cache_.begin(), cache_, eltit, std::next(eltit));
         
         eltit->rrip = RRIPval_NEAR;
         return true;
     }    
+
     void print_cache() {
+        std::cout << "\n";
         for (auto k = cache_.begin(); k != cache_.end(); k++) {
             std::cout << k->value << "(" << k->rrip << ") ";
         }
-        if (fst_dist != nullptr)
-            std::cout << fst_dist->value << std::endl;
-
+        if (fst_dist != cache_.end())
+            std::cout << fst_dist->value;
     }
 
 };
